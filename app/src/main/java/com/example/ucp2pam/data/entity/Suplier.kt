cd35c89964743b985beeb5736c47b43b36ccb6e6
ImamDwiYanPermana <imampermana244@gmail.com>
2024-12-23 22:54:17 +0700

    menambahkan sup di setiap nama atribut

--- app/src/main/java/com/example/ucp2pam/data/entity/Suplier.kt
+++ app/src/main/java/com/example/ucp2pam/data/entity/Suplier.kt
@@ -6,8 +6,8 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "suplier")
 data class Suplier(
     @PrimaryKey
-    val id: String,
-    val nama: String,
-    val kontak: String,
-    val alamat: String,
+    val idSup: String,
+    val namaSup: String,
+    val kontakSup: String,
+    val alamatSup: String,
 )
