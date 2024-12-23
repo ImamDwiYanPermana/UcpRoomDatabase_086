33f441c1ba78d7390f1f0e6f9c5438d297ce7e8e
ImamDwiYanPermana <imampermana244@gmail.com>
2024-12-23 22:56:13 +0700

    menghapus space

--- app/src/main/java/com/example/ucp2pam/data/repository/RepositoryBrg.kt
+++ app/src/main/java/com/example/ucp2pam/data/repository/RepositoryBrg.kt
@@ -5,12 +5,8 @@ import kotlinx.coroutines.flow.Flow
 
 interface RepositoryBrg {
     suspend fun  insertBrg(barang: Barang)
-
     fun getAllBrg(): Flow<List<Barang>>
-
-    fun getBrg(id: String): Flow<Barang>
-
+    fun getBrg(nama: String): Flow<Barang>
     suspend fun deleteBrg (barang: Barang)
-
     suspend fun updateBrg (barang: Barang)
 }
