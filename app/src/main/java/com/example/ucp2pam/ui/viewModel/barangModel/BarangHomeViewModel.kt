4c89b162bc93512825aadbfcfc23b0c92a817c0d
ImamDwiYanPermana <imampermana244@gmail.com>
2024-12-24 13:34:26 +0700

    Membuat clas HomeBrgViewModel dan membuat data class HomeBrgUiState

---
+++ app/src/main/java/com/example/ucp2pam/ui/viewModel/barangModel/BarangHomeViewModel.kt
@@ -0,0 +1,53 @@
+package com.example.ucp2pam.ui.viewModel.barangModel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.ucp2pam.data.entity.Barang
+import com.example.ucp2pam.data.repository.RepositoryBrg
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+
+class HomeBrgViewModel(
+    private val repositoryBrg: RepositoryBrg
+) : ViewModel() {
+    val HomeBrgUiState: StateFlow<HomeBrgUiState> = repositoryBrg.getAllBarang()
+        .filterNotNull()
+        .map {
+            HomeBrgUiState(
+                listBrg = it.toList(),
+                isLoading = false,
+            )
+        }
+        .onStart {
+            emit(HomeBrgUiState(isLoading = true))
+            delay(900)
+        }
+        .catch {
+            emit(
+                HomeBrgUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi Kesalahan"
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = HomeBrgUiState()
+        )
+
+}
+
+data class HomeBrgUiState (
+    val listBrg: List<Barang> = listOf(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = ""
+)
