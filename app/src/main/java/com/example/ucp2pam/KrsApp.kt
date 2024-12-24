c7a5c048a880445e17ec62e41b37c676a91d0d11
ImamDwiYanPermana <imampermana244@gmail.com>
2024-12-24 13:26:03 +0700

    KrsApp adalah subclass dari Application, yang merupakan titik masuk utama untuk aplikasi Android.

--- app/src/main/java/com/example/ucp2pam/KrsApp.kt
+++ app/src/main/java/com/example/ucp2pam/KrsApp.kt
@@ -1,4 +1,13 @@
 package com.example.ucp2pam
 
-class KrsApp {
+import android.app.Application
+import com.example.ucp2pam.data.dependenciesinjection.ContainerApp
+
+class KrsApp: Application() {
+    lateinit var containerApp: ContainerApp
+
+    override fun onCreate() {
+        super.onCreate()
+        containerApp = ContainerApp(this)
+    }
 }
