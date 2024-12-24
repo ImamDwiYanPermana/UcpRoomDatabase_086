f8418e769aa75e74c372920f7f10b7d66596dd10
ImamDwiYanPermana <imampermana244@gmail.com>
2024-12-24 11:14:20 +0700

    membuat table suplier

--- app/src/main/java/com/example/ucp2pam/data/entity/Suplier.kt
+++ app/src/main/java/com/example/ucp2pam/data/entity/Suplier.kt
@@ -2,12 +2,11 @@ package com.example.ucp2pam.data.entity
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-
 @Entity(tableName = "suplier")
 data class Suplier(
     @PrimaryKey
-    val idSup: String,
-    val namaSup: String,
-    val kontakSup: String,
-    val alamatSup: String,
+    val id: String,
+    val nama: String,
+    val kontak: String,
+    val alamat: String,
 )
